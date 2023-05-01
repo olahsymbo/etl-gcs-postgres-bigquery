dfd3ace0f6c11fd360a81e8d9c3def83c364810e
olahsymbo <olahsymbo@gmail.com>
2023-05-01 14:51:13 +0100

    added docker

--- core/pipeline/transform.py
+++ core/pipeline/transform.py
@@ -4,13 +4,12 @@ import pandas as pd
 class Transform:
     MERGE_KEY = 'customer_id'
 
-    def __init__(self, **kwargs):
-        self.df1 = kwargs.get('df1')
-        self.df2 = kwargs.get('df2')
-        self.df3 = kwargs.get('df3')
+    def __init__(self, df1, df2, df3):
+        self.df1 = df1
+        self.df2 = df2
+        self.df3 = df3
 
     def merge_transform(self):
-        merged_df = pd.merge(self.df1, self.df2, on='customer_id')
-        merged_df = pd.merge(merged_df, self.df3, on='customer_id')
-
+        merged_df = pd.merge(self.df1, self.df2, on=self.MERGE_KEY)
+        merged_df = pd.merge(merged_df, self.df3, on=self.MERGE_KEY)
         return merged_df
